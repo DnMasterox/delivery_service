2fa64db95f3262c41ba3bfc069d5dfd800e77f40
Mykola Shumakov <mastershumakov@gmail.com>
2020-05-01 19:34:31 +0300

    Rubocop fixes finished

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# class CouriersController
 class CouriersController < ApplicationController
-  before_action :set_courier, only: [:show, :edit, :update, :destroy]
+  before_action :set_courier, only: %i[show edit update destroy]
 
   # GET /couriers
   # GET /couriers.json
@@ -9,8 +12,7 @@ class CouriersController < ApplicationController
 
   # GET /couriers/1
   # GET /couriers/1.json
-  def show
-  end
+  def show; end
 
   # GET /couriers/new
   def new
@@ -18,8 +20,7 @@ class CouriersController < ApplicationController
   end
 
   # GET /couriers/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /couriers
   # POST /couriers.json
@@ -27,13 +28,7 @@ class CouriersController < ApplicationController
     @courier = Courier.new(courier_params)
 
     respond_to do |format|
-      if @courier.save
-        format.html { redirect_to @courier, notice: 'Courier was created.' }
-        format.json { render :show, status: :created, location: @courier }
-      else
-        format.html { render :new }
-        format.json { render json: @courier.errors, status: :unprocessable_entity }
-      end
+      save_courier(format)
     end
   end
 
@@ -41,15 +36,7 @@ class CouriersController < ApplicationController
   # PATCH/PUT /couriers/1.json
   def update
     respond_to do |format|
-      if @courier.update(courier_params)
-        format.html { redirect_to @courier, notice: 'Courier was updated.' }
-        format.json { render :show, status: :ok, location: @courier }
-      else
-        format.html { render :edit }
-        format.json do
-          render json: @courier.errors, status: :unprocessable_entity
-        end
-      end
+      update_courier(format)
     end
   end
 
@@ -73,5 +60,29 @@ class CouriersController < ApplicationController
   # Only allow a list of trusted parameters through.
   def courier_params
     params.require(:courier).permit(:name, :email)
+  end
+
+  def save_courier(format)
+    if @courier.save
+      format.html { redirect_to @courier, notice: 'Courier was created.' }
+      format.json { render :show, status: :created, location: @courier }
+    else
+      format.html { render :new }
+      format.json do
+        render json: @courier.errors, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def update_courier(format)
+    if @courier.update(courier_params)
+      format.html { redirect_to @courier, notice: 'Courier was updated.' }
+      format.json { render :show, status: :ok, location: @courier }
+    else
+      format.html { render :edit }
+      format.json do
+        render json: @courier.errors, status: :unprocessable_entity
+      end
+    end
   end
 end
