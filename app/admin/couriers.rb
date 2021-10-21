854957b85911c527e8a45843d00ebff7e1e472dc
nickshumak <mastershumakov@gmail.com>
2021-10-21 19:43:55 +0300

    active storage setup

--- app/admin/couriers.rb
+++ app/admin/couriers.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Courier do
-  permit_params :name, :email, :password, :password_confirmation, :validity, packages: []
+  permit_params :name, :email, :password, :password_confirmation, :validity, :avatar, packages: []
 
   index do
     selectable_column
     column :name
     column :email
     column :packages
+    image_tag url_for(:avatar), size: "200x200" if avatar.attached?
     column 'Valid', :validity
-    # column "Image" do |courier|
-    #   image_tag(courier.avatar,width:100,height:80)
-    # end
-    column do
-      input :avatar, as: :file
-    end
-    #image_column 'Document', :avatar, style: :thumb
     actions
   end
   show do
     attributes_table do
-      row "Image" do |courier|
-        image_tag(courier.avatar,width:100,height:80)
+      row :avatar do |ad|
+        image_tag url_for(ad.avatar) if ad.avatar.attached?
       end
       table_for courier do
         toggle_bool_column 'Valid', :validity
@@ -42,9 +36,7 @@ ActiveAdmin.register Courier do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.inputs "avatar" do
-        f.input :avatar, as: :file
-      end
+      f.input :avatar, as: :file
     end
     f.actions
   end
